e8ab4feeaddc956240d6a6b434406714372d135e
Tushar Saini <tusharsaini.in@gmail.com>
2026-03-03 23:36:35 +0530

    Developer (#18)
    * feat: add session model with schema and instance method for session activity
    
    * feat: add token model with schema for user authentication tokens
    
    * feat: add resumes collection model with schema for user resumes (#8)
    
    * Feature/backend models (#10)
    
    * feat: add resumes collection model with schema for user resumes
    
    * feat: add resume analysis model with schema for resume evaluation and English problem handling
    
    * Feature/backend models (#13)
    
    * feat: add resumes collection model with schema for user resumes
    
    * feat: add resume analysis model with schema for resume evaluation and English problem handling
    
    * feat: add layout analysis model with nested schemas for font, spacing, column, margin, and alignment analysis
    
    * feat: add job description model with schema for user job descriptions and skills validation
    
    * Feature/backend models (#15)
    
    * feat: add resumes collection model with schema for user resumes
    
    * feat: add resume analysis model with schema for resume evaluation and English problem handling
    
    * feat: add layout analysis model with nested schemas for font, spacing, column, margin, and alignment analysis
    
    * feat: add job description model with schema for user job descriptions and skills validation
    
    * feat: add chat message, chat session, and job match analysis models with schemas for messaging and job analysis
    
    * Feature/user controller (#17)
    
    * feat: add resumes collection model with schema for user resumes
    
    * feat: add resume analysis model with schema for resume evaluation and English problem handling
    
    * feat: add layout analysis model with nested schemas for font, spacing, column, margin, and alignment analysis
    
    * feat: add job description model with schema for user job descriptions and skills validation
    
    * feat: add chat message, chat session, and job match analysis models with schemas for messaging and job analysis
    
    * feat: implement user registration with session management and add necessary configurations

--- Backend/src/constants.js
+++ Backend/src/constants.js
@@ -2,3 +2,9 @@ export const DB_NAME = 'ResumeSaathiDB';
 export const SIZE_LIMIT = 16 * 1024; //16KB
 export const URL_LIMIT = 1 * 1024 * 1024; //1MB
 export const PARAMETER_LIMIT = 1000;
+export const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'strict',
+  maxAge: 365 * 24 * 60 * 60 * 1000, //1 year
+};
