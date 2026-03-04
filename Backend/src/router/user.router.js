0dbb8eb9fbf8ba744bbff722eb19a20e3b81853c
Tushar Saini <tusharsaini.in@gmail.com>
2026-03-04 19:14:01 +0530

    feat: implement password reset and change password functionality, add JWT verification middleware, and update user routes

--- Backend/src/router/user.router.js
+++ Backend/src/router/user.router.js
@@ -1,10 +1,17 @@
 import router from 'express';
-
+import verifyJwt from '../middlewares/auth.middleware.js';
 import {
   createUser,
   verifyEmail,
   resendVerificationEmail,
   loginUser,
+  logoutAllSessions,
+  logoutUser,
+  killSession,
+  listSessions,
+  forgotPassword,
+  resetPassword,
+  changePassword,
 } from '../controllers/user.controller.js';
 
 const userRouter = router.Router();
@@ -13,5 +20,12 @@ userRouter.post('/register', createUser);
 userRouter.get('/verify-email', verifyEmail);
 userRouter.post('/resend-verification-email', resendVerificationEmail);
 userRouter.post('/login', loginUser);
+userRouter.post('forgot-password', forgotPassword);
+userRouter.post('/reset-password', resetPassword);
+userRouter.post('/change-password', verifyJwt, changePassword);
+userRouter.get('/sessions', verifyJwt, listSessions);
+userRouter.post('/logout', verifyJwt, logoutUser);
+userRouter.post('/logout-all', verifyJwt, logoutAllSessions);
+userRouter.get('/kill-specific-session', verifyJwt, killSession);
 
 export default userRouter;
