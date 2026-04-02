ffc0cb82b1b09c99841ba60d789eed078c4dc7c9
Tushar Saini <tusharsaini.in@gmail.com>
2026-04-02 17:39:19 +0530

    Update sameSite attribute in COOKIE_OPTIONS for better security handling

--- Backend/src/constants.js
+++ Backend/src/constants.js
@@ -5,7 +5,7 @@ export const PARAMETER_LIMIT = 1000;
 export const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   maxAge: 365 * 24 * 60 * 60 * 1000, //1 year
 };
 export const EMAIL_VERIFICATION_TTL = 15 * 60 * 1000; //15 minutes
