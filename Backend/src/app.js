f03b80e09f8c43f9e038b0cf8aa3aa3baadac807
Tushar Saini <tusharsaini.in@gmail.com>
2026-05-04 17:36:20 +0530

    Refactor CORS configuration to use CLIENT_URLhttp://localhost:5173/ (#75)
    * Remove console logs for cleaner code and improved performance
    
    * Refactor CORS configuration and comment out unused allowedOrigins logic
    
    * Refactor CORS configuration to use CLIENT_URL constant for improved readability
    
    * Remove unused CORS options and DB_NAME constant for cleaner code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -22,13 +22,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// CORS configuration
+const CLIENT_URL = process.env.CLIENT_URLS;
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URLS,
-    optionsSuccessStatus: 200,
+    origin: CLIENT_URL,
+    credentials: true,
   })
 );
+
 
 // Body parsers
 app.use(express.json({ limit: SIZE_LIMIT }));
